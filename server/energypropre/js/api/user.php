4a69b97530883f3a81a3b4cfc8c506a7dd7f3643
Achraf DAHMANI <achraf.dahmani@ig2i.fr>
2015-07-01 12:00:58 +0200

    cleanup

--- server/energypropre/js/api/user.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$user=json_decode(file_get_contents('php://input'));  //get user from 
-	if($user->mail== "ok" && $user->pass== "ok") {
-		print "sucess";
-	}
-	else{
-		print "error";
-	}
-?>
